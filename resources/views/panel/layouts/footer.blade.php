f10ba61ea8ed8dcb94a04a39b3638baa3be29cbd
Albert Caba <albert22caba@gmail.com>
2024-07-27 02:04:25 -0400

    Eliminar secciones no utilizables, separar por partes la plantilla, con sus layouts y recursos. Continuar integrando bootstrap, js y demás archivos

---
+++ resources/views/panel/layouts/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="d-footer">
+    <div class="row align-items-center justify-content-between">
+        <div class="col-auto">
+            <p class="mb-0">© 2024 WowDash. All Rights Reserved.</p>
+        </div>
+        <div class="col-auto">
+            <p class="mb-0">Made by <span class="text-primary-600">wowtheme7</span></p>
+        </div>
+    </div>
+</footer>
+</main>
+
+<!-- jQuery library js -->
+<script src="{{ asset('import/assets/js/lib/jquery-3.7.1.min.js') }}"></script>
+<!-- Bootstrap js -->
+<script src="{{ asset('import/assets/js/lib/bootstrap.bundle.min.js') }}"></script>
+<!-- Apex Chart js -->
+<script src="{{ asset('import/assets/js/lib/apexcharts.min.js') }}"></script>
+<!-- Data Table js -->
+<script src="{{ asset('import/assets/js/lib/dataTables.min.js') }}"></script>
+<!-- Iconify Font js -->
+<script src="{{ asset('import/assets/js/lib/iconify-icon.min.js') }}"></script>
+<!-- jQuery UI js -->
+<script src="{{ asset('import/assets/js/lib/jquery-ui.min.js') }}"></script>
+<!-- Vector Map js -->
+<script src="{{ asset('import/assets/js/lib/jquery-jvectormap-2.0.5.min.js') }}"></script>
+<script src="{{ asset('import/assets/js/lib/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- Popup js -->
+<script src="{{ asset('import/assets/js/lib/magnifc-popup.min.js') }}"></script>
+<!-- Slick Slider js -->
+<script src="{{ asset('import/assets/js/lib/slick.min.js') }}"></script>
+<!-- main js -->
+<script src="{{ asset('import/assets/js/app.js') }}"></script>
+
+<script src="{{ asset('import/assets/js/homeOneChart.js') }}"></script>
---
+++ resources/views/panel/layouts/navigation.blade.php
@@ -0,0 +1,159 @@
+<div class="sidebar-menu-area">
+    <ul class="sidebar-menu" id="sidebar-menu">
+        
+        <li class="sidebar-menu-group-title">Application</li>
+        <li>
+            <a href="email.html">
+                <iconify-icon icon="mage:email" class="menu-icon"></iconify-icon>
+                <span>Email</span>
+            </a>
+        </li>
+        <li>
+            <a href="chat-message.html">
+                <iconify-icon icon="bi:chat-dots" class="menu-icon"></iconify-icon>
+                <span>Chat</span>
+            </a>
+        </li>
+        <li>
+            <a href="calendar-main.html">
+                <iconify-icon icon="solar:calendar-outline" class="menu-icon"></iconify-icon>
+                <span>Calendar</span>
+            </a>
+        </li>
+        <li class="dropdown">
+            <a href="javascript:void(0)">
+                <iconify-icon icon="hugeicons:invoice-03" class="menu-icon"></iconify-icon>
+                <span>Invoice</span>
+            </a>
+            <ul class="sidebar-submenu">
+                <li>
+                    <a href="invoice-list.html"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>
+                        List</a>
+                </li>
+                <li>
+                    <a href="invoice-preview.html"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i>
+                        Preview</a>
+                </li>
+                <li>
+                    <a href="invoice-add.html"><i class="ri-circle-fill circle-icon text-info-main w-auto"></i> Add
+                        new</a>
+                </li>
+                <li>
+                    <a href="invoice-edit.html"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i>
+                        Edit</a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="sidebar-menu-group-title">Application</li>
+
+        <li class="dropdown">
+            <a href="javascript:void(0)">
+                <iconify-icon icon="heroicons:document" class="menu-icon"></iconify-icon>
+                <span>Forms</span>
+            </a>
+            <ul class="sidebar-submenu">
+                <li>
+                    <a href="form.html"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i> Input
+                        Forms</a>
+                </li>
+                <li>
+                    <a href="form-layout.html"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i> Input
+                        Layout</a>
+                </li>
+                <li>
+                    <a href="form-validation.html"><i class="ri-circle-fill circle-icon text-success-main w-auto"></i>
+                        Form Validation</a>
+                </li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a href="javascript:void(0)">
+                <iconify-icon icon="mingcute:storage-line" class="menu-icon"></iconify-icon>
+                <span>Table</span>
+            </a>
+            <ul class="sidebar-submenu">
+                <li>
+                    <a href="table-basic.html"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>
+                        Basic Table</a>
+                </li>
+                <li>
+                    <a href="table-data.html"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i> Data
+                        Table</a>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="widgets.html">
+                <iconify-icon icon="fe:vector" class="menu-icon"></iconify-icon>
+                <span>Widgets</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="/panel/add-estudiante">
+                <iconify-icon icon="flowbite:users-group-outline" class="menu-icon"></iconify-icon>
+                <span>Estudiantes</span>
+            </a>
+        </li>
+
+        <li class="sidebar-menu-group-title">Application</li>
+
+        <li class="dropdown">
+            <a href="javascript:void(0)">
+                <iconify-icon icon="simple-line-icons:vector" class="menu-icon"></iconify-icon>
+                <span>Authentication</span>
+            </a>
+            <ul class="sidebar-submenu">
+                <li>
+                    <a href="sign-in.html"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i> Sign
+                        In</a>
+                </li>
+                <li>
+                    <a href="sign-up.html"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i> Sign
+                        Up</a>
+                </li>
+                <li>
+                    <a href="forgot-password.html"><i class="ri-circle-fill circle-icon text-info-main w-auto"></i>
+                        Forgot Password</a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="dropdown">
+            <a href="javascript:void(0)">
+                <iconify-icon icon="icon-park-outline:setting-two" class="menu-icon"></iconify-icon>
+                <span>Settings</span>
+            </a>
+            <ul class="sidebar-submenu">
+                <li>
+                    <a href="company.html"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>
+                        Company</a>
+                </li>
+                <li>
+                    <a href="notification.html"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i>
+                        Notification</a>
+                </li>
+                <li>
+                    <a href="notification-alert.html"><i class="ri-circle-fill circle-icon text-info-main w-auto"></i>
+                        Notification Alert</a>
+                </li>
+                <li>
+                    <a href="theme.html"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i> Theme</a>
+                </li>
+                <li>
+                    <a href="currencies.html"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i>
+                        Currencies</a>
+                </li>
+                <li>
+                    <a href="language.html"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i>
+                        Languages</a>
+                </li>
+                <li>
+                    <a href="payment-gateway.html"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i>
+                        Payment Gateway</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
